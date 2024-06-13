5fb9df090035d0fa9b2d192e1976217540f642bd
rj7604 <161802512+rj7604@users.noreply.github.com>
2024-06-14 02:49:59 +0530

    Update ETH_Proof_Assessment.sol

--- ETH_Proof_Assessment.sol
+++ ETH_Proof_Assessment.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract MyToken {
+contract MyToken 
+{
     // Public variables
     string public Name;
     string public ID;
@@ -11,14 +12,16 @@ contract MyToken {
     mapping (address => uint) public Balances;
 
     // Constructor to initialize the contract
-    constructor() {
+    constructor() 
+    {
         Name = "RISHABH JAIN";
         ID = "50095";
         Total_Supply = 0;
     }
 
     // Mint function
-    function Mint(address _address, uint _amount) public {
+    function Mint(address _address, uint _amount) public 
+    {
         for (uint i = 0; i < _amount; i++) {
             Total_Supply++;
            Balances[_address]++;
@@ -26,11 +29,12 @@ contract MyToken {
     }
 
     // Burn function 
-    function Burn(address _address, uint _amount) public {
+    function Burn(address _address, uint _amount) public 
+    {
         require(Balances[_address] >= _amount, "Insufficient balance");
         for (uint i = 0; i < _amount; i++) {
             Total_Supply--;
             Balances[_address]--;
-        }
-    }
+            }
+        }
 }
